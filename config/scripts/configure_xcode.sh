92f101c3e46936ff0856e940c1badb658bd65d6e
Michael Chaffee <mchaffee@thoughtworks.com>
2016-04-11 09:01:22 -0700

    OK

---
+++ config/scripts/configure_xcode.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
+
+ln -s ../../plugin/dracula-theme/xcode/Dracula.dvtcolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/Dracula.dvtcolortheme
