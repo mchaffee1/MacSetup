bccfc8242d192304a322b2c17f81e54d901b9c0d
Michael Chaffee <mchaffee@thoughtworks.com>
2016-10-19 09:44:42 -0700

    add git spsp alias

--- config/scripts/configure_git.sh
+++ config/scripts/configure_git.sh
@@ -10,15 +10,17 @@ git config --global format.pretty format:"%h %C(yellow)%ci%C(reset) %C(green)%an
 
 git config --global alias.aa 'add .'
 git config --global alias.ap 'add -p'
+git config --global alias.cgrep '!f() { git log --format=%H --grep=$1; }; f'
 git config --global alias.cm 'commit -m'
 git config --global alias.co checkout
+git config --global alias.cshow '!f() { git show `git cgrep $1`; }; f'
 git config --global alias.dc 'diff --cached'
 git config --global alias.dh 'diff head'
-git config --global alias.pr 'pull --rebase'
-git config --global alias.rc 'rebase --continue'
-git config --global alias.st status
+git config --global alias.logs 'log -n 20'
 git config --global alias.lp 'log -p'
 git config --global alias.ls 'log --stat'
-git config --global alias.logs 'log -n 20'
-git config --global alias.cgrep '!f() { git log --format=%H --grep=$1; }; f'
-git config --global alias.cshow '!f() { git show `git cgrep $1`; }; f'
+git config --global alias.pr 'pull --rebase'
+git config --global alias.rc 'rebase --continue'
+git config --global alias.spsp 'stash && pull && stash pop'
+git config --global alias.st status
+
