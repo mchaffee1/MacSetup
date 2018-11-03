2d27d2bbf7a17fa0139a123d19a5348d0e96d2ee
Michael Chaffee <mchaffee@gmail.com>
2018-11-03 13:09:32 -0700

    sign git commits

--- config/scripts/configure_git.sh
+++ config/scripts/configure_git.sh
@@ -8,14 +8,15 @@ git config --global log.decorate true
 git config --global gitsh.defaultCommand "!clear && logs && !echo && st"
 git config --global format.pretty format:"%h %C(yellow)%ci%C(reset) %C(green)%an%C(reset) |%C(yellow)%C(bold)%d%C(reset) %s"
 
+commit='commit -S'
 git config --global alias.a 'add'
 git config --global alias.aa 'add .'
 git config --global alias.ap 'add -p'
-git config --global alias.ca 'commit --amend'
-git config --global alias.acane 'add . && commit --amend --no-edit'
-git config --global alias.cane 'commit --amend --no-edit'
+git config --global alias.ca "$commit --amend"
+git config --global alias.acane "add . && $commit --amend --no-edit"
+git config --global alias.cane "$commit --amend --no-edit"
 git config --global alias.cgrep '!f() { git log --format=%H --grep=$1; }; f'
-git config --global alias.cm 'commit -m'
+git config --global alias.cm "$commit -m"
 git config --global alias.co checkout
 git config --global alias.cop 'checkout -p'
 git config --global alias.cshow '!f() { git show `git cgrep $1`; }; f'
@@ -24,6 +25,7 @@ git config --global alias.dh 'diff head'
 git config --global alias.ds 'diff --stat'
 git config --global alias.ff 'merge --ff-only'
 git config --global alias.logs 'log -n 20'
+git config --global alias.lg 'log --show-signature'
 git config --global alias.lp 'log -p'
 git config --global alias.ls 'log --stat'
 git config --global alias.mf 'merge --ff-only'
