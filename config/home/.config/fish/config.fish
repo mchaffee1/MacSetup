b9d1b6885812009fe6126bd6d5ed8c900c8eb819
Michael Chaffee <mchaffee@thoughtworks.com>
2022-07-01 09:46:58 -0400

    Change to use brew-file

--- config/home/.config/fish/config.fish
+++ config/home/.config/fish/config.fish
@@ -1,12 +1,17 @@
+# Aliases
 source ~/.alias
 
+# Load machine-specific proxy info
 if test -e ~/proxies.sh
     source ~/proxies.sh
 end
 
-# source (rbenv init - | psub)
+# Fire up brew-wrap
+if test -f (brew --prefix)/etc/brew-wrap.fish
+  source (brew --prefix)/etc/brew-wrap.fish
+end
 
 set -x GPG_TTY (tty)
 
 pyenv init - | source
-
+# source (rbenv init - | psub)
--- config/scripts/brew_initial.sh
+++ config/scripts/brew_initial.sh
@@ -5,4 +5,11 @@ sudo xcode-select --install 2>/dev/null
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew bundle
+# install homebrew-file (https://github.com/rcmdnk/homebrew-file)
+brew install rcmdnk/file/brew-file
+
+# hook to GitHub
+brew file set_repo -r git@github.com:mchaffee1/MacSetup.git
+
+# Install packages
+brew file install
